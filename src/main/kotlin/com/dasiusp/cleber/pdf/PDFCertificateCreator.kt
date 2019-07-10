b326da89e5bb9161ba52b487a1e08a9e54ff7cf5
sweetbakery <44716827+sweetbakery@users.noreply.github.com>
2019-07-09 23:06:27 -0300

    Incrementa geração de PDF com mais dados (#27)
    Closes #22

---
+++ src/main/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreator.kt
@@ -0,0 +1,101 @@
+package com.dasiusp.cleber.pdf
+
+import com.dasiusp.cleber.pdf.PDFFont.bodyFont
+import com.dasiusp.cleber.pdf.PDFFont.bottomFont
+import com.dasiusp.cleber.pdf.PDFFont.titleFont
+import com.dasiusp.cleber.pdf.PDFFont.tokenFont
+import com.itextpdf.text.*
+import com.itextpdf.text.pdf.PdfWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+data class Certificate(
+    val personName: String,
+    val date: LocalDate,
+    val eventType: String,
+    val eventName: String,
+    val duration: Int,
+    val token: String
+)
+
+class PDFCertificateCreator(
+    private val outputDirectory: String
+) {
+
+    init {
+        createOutputDirectory()
+    }
+
+    private fun createOutputDirectory() {
+        File(outputDirectory).mkdirs()
+    }
+
+    fun createPdf(certificate: Certificate) {
+        val targetFile = File(outputDirectory, "${certificate.token}.pdf")
+        writeDocument(targetFile, certificate)
+    }
+
+    private fun writeDocument(targetFile: File, certificate: Certificate) {
+        val document = Document()
+        PdfWriter.getInstance(document, FileOutputStream(targetFile))
+
+        document.use {
+            addTitle()
+            addBody(certificate)
+            addPlaceandDate(certificate)
+            addToken(certificate)
+        }
+    }
+
+    private fun Document.addTitle() {
+        add(writeParagraph("Certificado de Participação", titleFont, Element.ALIGN_CENTER))
+    }
+
+    private fun Document.addBody(certificate: Certificate) {
+        add(
+            writeParagraph(
+                "Certificamos que ${certificate.personName} participou do evento ${certificate.eventType} ${certificate.eventName} realizado na Escola de Artes Ciências e Humanidades da Universidade de São Paulo EACH-USP, com duração de ${certificate.duration} horas.",
+                bodyFont,
+                Element.ALIGN_JUSTIFIED
+            )
+        )
+    }
+
+    private fun Document.addPlaceandDate(certificate: Certificate) {
+        add(writeParagraph("São Paulo, ${certificate.date.formatDate()}.", bottomFont, Element.ALIGN_CENTER))
+    }
+
+    private fun Document.addToken(certificate: Certificate) {
+        add(writeParagraph("${certificate.token}", tokenFont, Element.ALIGN_CENTER))
+    }
+
+    private fun LocalDate.formatDate(): String {
+        return format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    }
+
+    private fun writeParagraph(text: String, font: Font, alignment: Int): Paragraph {
+        val paragraph = Paragraph(text, font)
+        paragraph.alignment = alignment
+        return paragraph
+    }
+
+    private fun Document.use(block: Document.() -> Unit) {
+        open()
+        block()
+        close()
+    }
+}
+
+private object PDFFont {
+    val titleFont = registerFont("merriweather", "merriweatherfont", 24f)
+    val bodyFont = registerFont("arial", "arialfont", 18f)
+    val bottomFont = registerFont("arial", "arialfont", 14f)
+    val tokenFont = registerFont("bold", "boldfont", 14f)
+
+    private fun registerFont(fontName: String, alias: String, size: Float): Font {
+        FontFactory.register("assets/fonts/$fontName.ttf", alias)
+        return FontFactory.getFont(alias, size)
+    }
+}
--- src/test/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreatorTest.kt
+++ src/test/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreatorTest.kt
@@ -7,32 +7,38 @@ import io.kotlintest.specs.FunSpec
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
+import java.time.LocalDate
 
-class HelloWorldPdfGeneratorTest : FunSpec() {
-    
+class PDFCertificateCreatorTest : FunSpec() {
+
     private val testDirectory = "out/test/test"
+    private val certificate =
+        Certificate("Joao Joanino da Silva Pereira", LocalDate.of(2019, 7, 20), "Palestra", "do Jorge", 300, "FooBar")
 
     init {
         test("Should create file in the specified directory") {
             createFooBarPdf()
             File(testDirectory).shouldContainFile("FooBar.pdf")
         }
-        
+
         test("Should write basic text to file") {
             createFooBarPdf()
-            
+
             val document = PDDocument.load(File(testDirectory, "FooBar.pdf"))
             val stripper = PDFTextStripper()
             val text = stripper.getText(document)
-            
-            text shouldContain "Hello, FooBar!"
+            text.withoutLinebreaks() shouldContain "Certificado de Participação Certificamos que Joao Joanino da Silva Pereira participou do evento Palestra do Jorge realizado na Escola de Artes Ciências e Humanidades da Universidade de São Paulo EACH-USP, com duração de 300 horas. São Paulo, 20/07/2019. FooBar"
         }
     }
-    
-    private fun createFooBarPdf() {
-        HelloWorldPdfGenerator(testDirectory).createPdf("FooBar")
+
+    private fun String.withoutLinebreaks(): String {
+        return replace("\r", "").replace("\n", " ")
     }
-    
+
+    private fun createFooBarPdf() {
+        PDFCertificateCreator(testDirectory).createPdf(certificate)
+    }
+
     override fun beforeTest(testCase: TestCase) {
         File(testDirectory).deleteRecursively()
     }
