7e493ac78e9ce3c1212e80eb9ebe5f87c1d35ad3
sweetbakery <44716827+sweetbakery@users.noreply.github.com>
2019-07-17 19:56:49 -0300

    Add images; Replace PDF library (#37)
    Closes #36

--- src/main/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreator.kt
+++ src/main/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreator.kt
@@ -1,20 +1,7 @@
 package com.dasiusp.cleber.pdf
 
-import com.dasiusp.cleber.pdf.PDFFont.bodyFont
-import com.dasiusp.cleber.pdf.PDFFont.bottomFont
-import com.dasiusp.cleber.pdf.PDFFont.titleFont
-import com.dasiusp.cleber.pdf.PDFFont.tokenFont
-import com.itextpdf.text.Document
-import com.itextpdf.text.Element
-import com.itextpdf.text.Font
-import com.itextpdf.text.FontFactory
-import com.itextpdf.text.PageSize
-import com.itextpdf.text.Paragraph
-import com.itextpdf.text.pdf.PdfWriter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.io.File
-import java.io.FileOutputStream
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -29,76 +16,26 @@ data class Certificate(
 
 @Component
 class PDFCertificateCreator(
-    @Value("\${certificate.output.directory}") private val certificateOutputDirectory: String,
-    @Value("\${certificate.title.text}") private val certificateTitleText: String,
-    @Value("\${certificate.body.text}") private val certificateBodyText: String,
-    @Value("\${certificate.place.and.date.text}") private val certificatePlaceAndDateText: String,
-    @Value("\${certificate.token.text}") private val certificateTokenText: String
+    @Value("\${certificate.title}") private val certificateTitleText: String,
+    @Value("\${certificate.body}") private val certificateBodyText: String,
+    @Value("\${certificate.place.and.date}") private val certificatePlaceAndDateText: String,
+    @Value("\${certificate.token}") private val certificateTokenText: String
 ) {
-
-    init {
-        createOutputDirectory()
-    }
-
-    private fun createOutputDirectory() {
-        File(certificateOutputDirectory).mkdirs()
-    }
-
-    fun createPdf(certificate: Certificate) {
-        val targetFile = File(certificateOutputDirectory, "${certificate.token}.pdf")
-        writeDocument(targetFile, certificate)
-    }
-
-    private fun writeDocument(targetFile: File, certificate: Certificate) {
-        val document = Document(PageSize.A4.rotate())
-        PdfWriter.getInstance(document, FileOutputStream(targetFile))
-        
+    
+    fun createPdf(certificate: Certificate): ByteArray {
         val replacer = CertificateTextReplacer(certificate)
+        val pdfCertificate = PDFCertificate(replacer.title, replacer.body, replacer.placeAndDate, replacer.token)
         
-        document.use {
-            addCertificateTitle(replacer.title)
-            addCertificateBody(replacer.body)
-            addCertificatePlaceAndDate(replacer.placeAndDate)
-            addCertificateToken(replacer.token)
-        }
+        return pdfCertificate.create()
     }
 
-    private fun Document.addCertificateTitle(title: String) {
-        add(writeParagraph(title, titleFont, Element.ALIGN_CENTER))
-    }
-
-    private fun Document.addCertificateBody(body: String) {
-        add(writeParagraph(body, bodyFont, Element.ALIGN_JUSTIFIED))
-    }
-    
-    private fun Document.addCertificatePlaceAndDate(placeAndDate: String) {
-        add(writeParagraph(placeAndDate, bottomFont, Element.ALIGN_CENTER))
-    }
-    
-    private fun Document.addCertificateToken(token: String) {
-        add(writeParagraph(token, tokenFont, Element.ALIGN_CENTER))
-    }
-    
-
-    private fun writeParagraph(text: String, font: Font, alignment: Int): Paragraph {
-        val paragraph = Paragraph(text, font)
-        paragraph.alignment = alignment
-        return paragraph
-    }
-
-    private fun Document.use(block: Document.() -> Unit) {
-        open()
-        block()
-        close()
-    }
-    
     private inner class CertificateTextReplacer(val certificate: Certificate) {
-        
+
         val title = certificateTitleText.withReplacedVariables()
         val body = certificateBodyText.withReplacedVariables()
         val placeAndDate = certificatePlaceAndDateText.withReplacedVariables()
         val token = certificateTokenText.withReplacedVariables()
-    
+
         private fun String.withReplacedVariables(): String {
             return replace("%NOME_PESSOA%", certificate.personName)
                 .replace("%TIPO_EVENTO%", certificate.eventType)
@@ -107,19 +44,7 @@ class PDFCertificateCreator(
                 .replace("%DATA%", certificate.date.formatDate())
                 .replace("%TOKEN%", certificate.token)
         }
-    
+
         private fun LocalDate.formatDate() = format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-    }
-}
-
-private object PDFFont {
-    val titleFont = registerFont("merriweather", "merriweatherfont", 24f)
-    val bodyFont = registerFont("arial", "arialfont", 18f)
-    val bottomFont = registerFont("arial", "arialfont", 14f)
-    val tokenFont = registerFont("bold", "boldfont", 14f)
-
-    private fun registerFont(fontName: String, alias: String, size: Float): Font {
-        FontFactory.register("assets/fonts/$fontName.ttf", alias)
-        return FontFactory.getFont(alias, size)
     }
 }
--- src/test/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreatorTest.kt
+++ src/test/kotlin/com/dasiusp/cleber/pdf/PDFCertificateCreatorTest.kt
@@ -1,62 +1,47 @@
 package com.dasiusp.cleber.pdf
 
-import io.kotlintest.TestCase
-import io.kotlintest.matchers.file.shouldContainFile
 import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.FunSpec
 import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.text.PDFTextStripper
-import java.io.File
 import java.time.LocalDate
 
 class PDFCertificateCreatorTest : FunSpec() {
-
-    private val testDirectory = "out/test/test"
+    
     private val certificate =
         Certificate("Joao Joanino da Silva Pereira", LocalDate.of(2019, 7, 20), "Palestra", "do Jorge", 300, "FooBar")
-
+    
     init {
-        test("Should create file in the specified directory") {
-            createFooBarPdf()
-            File(testDirectory).shouldContainFile("FooBar.pdf")
-        }
-
         test("Should write basic text to file") {
-            createFooBarPdf()
-            val document = loadDocument()
+            val bytes = createFooBarPdf()
+            val document = PDDocument.load(bytes)
             val stripper = PDFTextStripper()
             val text = stripper.getText(document)
             text.withoutLinebreaks() shouldContain "Certificado de participação Certificamos que Joao Joanino da Silva Pereira participou do evento Palestra do Jorge realizado na Escola de Artes Ciências e Humanidades da Universidade de São Paulo EACH-USP, com duração de 300 horas. São Paulo, 20/07/2019. FooBar"
         }
-
-        test("Should create file in landscape format") {
-            createFooBarPdf()
-            val document = loadDocument()
-            document.pages[0].rotation shouldBe 90
+        
+        test("Should create pdf in landscape format") {
+            val bytes = createFooBarPdf()
+            
+            val document = PDDocument.load(bytes)
+            document.pages[0].mediaBox.height shouldBe PDRectangle.A4.width
+            document.pages[0].mediaBox.width shouldBe PDRectangle.A4.height
         }
-
+        
     }
-
+    
     private fun String.withoutLinebreaks(): String {
-        return replace("\r", "").replace("\n", " ")
+        return replace("\r", "").replace("\n", " ").replace("   ", " ")
     }
-
-    private fun createFooBarPdf() {
-        PDFCertificateCreator(
-            testDirectory,
+    
+    private fun createFooBarPdf(): ByteArray {
+        return PDFCertificateCreator(
             "Certificado de participação",
             "Certificamos que %NOME_PESSOA% participou do evento %TIPO_EVENTO% %NOME_EVENTO% realizado na Escola de Artes Ciências e Humanidades da Universidade de São Paulo EACH-USP, com duração de %DURACAO% horas.",
             "São Paulo, %DATA%.",
             "%TOKEN%"
         ).createPdf(certificate)
-    }
-
-    private fun loadDocument(): PDDocument {
-        return PDDocument.load(File(testDirectory, "FooBar.pdf"))
-    }
-
-    override fun beforeTest(testCase: TestCase) {
-        File(testDirectory).deleteRecursively()
     }
 }
